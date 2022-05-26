df3d44c860d28856691aca98435d0b2ece02dc9e
Nelio Bila <neliobilla@gmail.com>
2022-05-26 18:03:53 +0200

    Feature: Create Article

--- resources/js/config/axios.js
+++ resources/js/config/axios.js
@@ -5,13 +5,13 @@ axios.defaults.headers.common["Authorization"] =
     "Bearer " + localStorage.getItem("op_token");
 
 // before a request is made start the nprogress
-axios.interceptors.request.use((config) => {
-    NProgress.start();
-    return config;
-});
+// axios.interceptors.request.use((config) => {
+//     NProgress.start();
+//     return config;
+// });
 
 // before a response is returned stop nprogress
-axios.interceptors.response.use((response) => {
-    NProgress.done();
-    return response;
-});
+// axios.interceptors.response.use((response) => {
+//     NProgress.done();
+//     return response;
+// });
