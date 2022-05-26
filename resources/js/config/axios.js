0a2a685324c4bf26f676e34605e7fc518cf8b75d
Nelio Bila <neliobilla@gmail.com>
2022-05-26 16:52:04 +0200

    Feature: Create Article

--- resources/js/config/axios.js
+++ resources/js/config/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/api/";
+axios.defaults.baseURL = process.env.MIX_APP_URL + "/api/";
 axios.defaults.headers.common["Authorization"] =
     "Bearer " + localStorage.getItem("op_token");
 
