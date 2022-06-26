da24f93bcc5a685d43cf556b4c8aac2245de277f
Nelio Bila <neliobilla@gmail.com>
2022-06-26 21:32:43 +0200

    Feat: News pages

---
+++ resources/js/pages/News/Orto.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <NavBar />
+    <div class="container">
+      <h1 class="text-center">Ortoprotesia</h1>
+      <p>
+        A Ortoprotesia é uma das 18 profissões que integram a carreira geral de
+        Técnico de Diagnóstico e Terapêutica (TDT). O ortoprotésico tem como
+        funções a avaliação de indivíduos com problemas motores ou posturais,
+        com a finalidade de conceber e aplicar os dispositivos necessários e
+        mais adequados à correcção do aparelho locomotor, ou à sua substituição
+        no caso de amputações, e de desenvolvimento de acções visando assegurar
+        a colocação de dispositivos fabricados e respectivo ajustamento, quando
+        necessário. A função do ortoprotésico baseia-se na construção, adaptação
+        e aplicação de próteses e ortóteses a indivíduos com amputação, ausência
+        por malformação congénita dos membros ou deficiência funcional total ou
+        parcial do sistema neuro-músculo-esquelético.
+      </p>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import NavBar from "../../components/NavBar.vue";
+import Footer from "../../components/Footer.vue";
+</script>
