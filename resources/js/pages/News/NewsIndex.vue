21037e640645d057ba74c76d94ecf08e849be44c
Nelio Bila <neliobilla@gmail.com>
2022-06-26 21:23:34 +0200

    Feat: News pages

--- resources/js/pages/News/NewsIndex.vue
+++ resources/js/pages/News/NewsIndex.vue
@@ -5,9 +5,17 @@
       <h1 class="text-center">
         Centro Ortopédico do Hospital Central de Maputo
       </h1>
+      <p>
+        O Centro Ortopédico do Hospital Central de Maputo localiza-se na Avenida
+        Eduardo Mondlane 1653, Maputo, nas instalações do Hospital Central de
+        Maputo, tem como missão oferecer serviços de reabilitação a pacientes
+        portadores de deficiência física através de aparelhos ortopédicos como
+        próteses, órteses, cadeiras de rodas, andarilhos, canadianas muletas,
+        canadianas axilares, bengalas e mais.
+      </p>
 
-      <h2>Fluxo de atendimento</h2>
-      <!-- <img src="" alt="" /> -->
+      <h2 class="text-center">Fluxo de atendimento</h2>
+      <img src="./images/various/fluxo.png" alt="fluxo de atendimento" />
     </div>
     <Footer />
   </div>
