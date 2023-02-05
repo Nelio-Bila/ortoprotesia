3262500a506385dc765502169b28b1f9e4ea6d13
Nelio Bila <neliobilla@gmail.com>
2023-02-05 18:15:38 +0200

    fix: privacy policy

--- resources/js/pages/legal/PrivacyPolicy.vue
+++ resources/js/pages/legal/PrivacyPolicy.vue
@@ -12,57 +12,76 @@
         a plataforma Ortoprotesia está a aceitar expressamente e automaticamente
         esta Política de Privacidade. Se discordar da mesma, no seu todo ou em
         parte, deve deixar de utilizar este serviço imediatamente e desactivar a
-        sua conta CONTA PROFISSIONAL DE SAÚDE. Para criar a conta e utilizar a
-        plaforma Ortoprotesia, o funcionário deve disponibilizar o seu nome e
-        apelido, palavra-passe, data de nascimento, profissão/serviço,
-        experiência e nível de educação. Estes campos são todos obrigatórios.
-        Esta informação, com excepção da palavra-passe, é disponibilizada para
-        todos os Utilizadores da plataforma Ortoprotesia, como parte do perfil
-        do Profissional de Saúde. A equipa de gestão do Plataforma Ortoprotesia
-        não é, de forma alguma, responsável pela utilização ou disseminação
-        destes dados por parte de outros Utilizadores do Plataforma
-        Ortoprotesia, devendo qualquer situação anómala ser tratada entre os
-        Utilizadores envolvidos CONTA UTENTE Para criar a sua conta e utilizar o
-        Plataforma Ortoprotesia, o Cliente deve disponibilizar o seu nome e
-        apelido, palavra-passe e contacto telefónico. Apenas o nome do Cliente é
-        disponibilizado para todos os Utilizadores do Plataforma Ortoprotesia
-        como parte do seu perfil. A equipa de gestão do Plataforma Ortoprotesia
-        não é, de forma alguma, responsável pela utilização ou disseminação
-        destes dados por parte de outros Utilizadores do Plataforma
-        Ortoprotesia, devendo qualquer situação anómala ser tratada entre os
-        Utilizadores envolvidos. CONTEÚDO PÚBLICO Para além da informação acima
-        referida, relativa aos perfis dos Trabalhadores e Clientes, conteúdos
-        submetidos por qualquer Utilizador, tais como avaliações, comentários,
-        fotos e outros, são disponibilizados imediatamente e sem intervenção de
-        terceiros para todos os Utilizadores do Plataforma Ortoprotesia. O
-        Plataforma Ortoprotesia não é, de forma alguma, responsável pela
-        natureza deste conteúdo e pela utilização ou disseminação deste por
-        parte de outros Utilizadores do Plataforma Ortoprotesia. PUBLICIDADE E
-        PROMOÇÕES Mensagens de publicidade e promoções do Plataforma
-        Ortoprotesia ou de terceiros (Anunciantes) podem ser enviadas aos
-        Utilizadores por SMS. O Utilizador pode optar por não receber estas
-        mensagens seguindo as instruções de auto-exclusão destas comunicações no
-        seu perfil (a.k.a. serviços de opt-in/ opt-out). Anúncios no website e
-        aplicativo móvel não são opcionais. ESTATÍSTICAS A equipa de gestão,
-        através do serviço Google Analytics, irá recolher informação
-        demográfica, comportamental e tecnológica dos Utilizadores do website do
-        Plataforma Ortoprotesia e das redes sociais referentes a este serviço
-        (Facebook, Twitter, etc.). A informação contida nos registos inclui, o
-        seu ISP (Internet Service Provider), o seu endereço IP (Internet
-        Protocol), o navegador que utilizou ao visitar o website e as redes
-        sociais referentes ao Plataforma Ortoprotesia, o tempo da sua visita e
-        que páginas visitou no website. Esta informação é anónima e privada, e
-        serve para monitorar a performance do site e melhorar o serviço para os
-        Utilizadores. O Plataforma Ortoprotesia reserva-se o direito de utilizar
-        esta informação à sua descrição, desde que este uso não infrinja a
-        privacidade dos Utilizadores deste serviço. SEGURANÇA A base de dados do
-        Plataforma Ortoprotesia, que compreende toda a informação submetida
-        pelos Utilizadores, está protegida pelos mais altos padrões de segurança
-        informática. Porém, o Plataforma Ortoprotesia não é, de forma alguma,
-        responsável pela furto ou uso indevido desta informação por parte de
-        outros Utilizadores ou por parte de terceiros. É dever do Utilizador
-        fazer o Logout (Desligar) a sua conta para terminar a sua sessão no
-        Plataforma Ortoprotesia e proteger os seus dados.
+        sua conta
+      </p>
+      <h5 class="text-left my-2">CONTA PROFISSIONAL DE SAÚDE.</h5>
+      <p>
+        Para criar a conta e utilizar a plaforma Ortoprotesia, o funcionário
+        deve disponibilizar o seu nome e apelido, palavra-passe, data de
+        nascimento, profissão/serviço, experiência e nível de educação. Estes
+        campos são todos obrigatórios. Esta informação, com excepção da
+        palavra-passe, é disponibilizada para todos os Utilizadores da
+        plataforma Ortoprotesia, como parte do perfil do Profissional de Saúde.
+        A equipa de gestão do Plataforma Ortoprotesia não é, de forma alguma,
+        responsável pela utilização ou disseminação destes dados por parte de
+        outros Utilizadores do Plataforma Ortoprotesia, devendo qualquer
+        situação anómala ser tratada entre os Utilizadores envolvidos
+      </p>
+      <h5 class="text-left my-2">CONTA UTENTE</h5>
+      <p>
+        Para criar a sua conta e utilizar a Plataforma Ortoprotesia, o utente
+        deve disponibilizar o seu nome, apelido, palavra-passe e contacto
+        telefónico. Apenas o nome do utente é disponibilizado para todos os
+        Utilizadores do Plataforma Ortoprotesia como parte do seu perfil. A
+        equipa de gestão do Plataforma Ortoprotesia não é, de forma alguma,
+        responsável pela utilização ou disseminação destes dados por parte de
+        outros Utilizadores da Plataforma Ortoprotesia, devendo qualquer
+        situação anómala ser tratada entre os utilizadores envolvidos.
+      </p>
+      <h5 class="text-left my-2">CONTEÚDO PÚBLICO</h5>
+      <p>
+        Para além da informação acima referida, relativa aos perfis dos
+        profissionais de saúde e utentes, conteúdos submetidos por qualquer
+        Utilizador, tais como avaliações, comentários, fotos e outros, são
+        disponibilizados imediatamente e sem intervenção de terceiros para todos
+        os Utilizadores do Plataforma Ortoprotesia. A Plataforma Ortoprotesia
+        não é, de forma alguma, responsável pela natureza deste conteúdo e pela
+        utilização ou disseminação deste por parte de outros Utilizadores da
+        Plataforma Ortoprotesia.
+      </p>
+      <h5 class="text-left my-2">PUBLICIDADE E PROMOÇÕES</h5>
+      <p>
+        Mensagens de publicidade e promoções da Plataforma Ortoprotesia ou de
+        terceiros (Anunciantes) podem ser enviadas aos Utilizadores por email. O
+        Utilizador pode optar por não receber estas mensagens seguindo as
+        instruções de auto-exclusão destas comunicações no seu email. Anúncios
+        no website não são opcionais.
+      </p>
+      <h5 class="text-left my-2">ESTATÍSTICAS</h5>
+      <p>
+        A equipa de gestão, através do serviço Google Analytics, irá recolher
+        informação demográfica, comportamental e tecnológica dos Utilizadores do
+        website do Plataforma Ortoprotesia e das redes sociais referentes a este
+        serviço (Facebook, Twitter, etc.). A informação contida nos registos
+        inclui, o seu ISP (Internet Service Provider), o seu endereço IP
+        (Internet Protocol), o navegador que utilizou ao visitar o website e as
+        redes sociais referentes a Plataforma Ortoprotesia, o tempo da sua
+        visita e que páginas visitou no website. Esta informação é anónima e
+        privada, e serve para monitorar a performance do site e melhorar o
+        serviço para os Utilizadores. A Plataforma Ortoprotesia reserva-se o
+        direito de utilizar esta informação à sua descrição, desde que este uso
+        não infrinja a privacidade dos Utilizadores deste serviço.
+      </p>
+
+      <h5 class="text-left my-2">SEGURANÇA</h5>
+      <p>
+        A base de dados da Plataforma Ortoprotesia, que compreende toda a
+        informação submetida pelos Utilizadores, está protegida pelos mais altos
+        padrões de segurança informática. Porém, a Plataforma Ortoprotesia não
+        é, de forma alguma, responsável pela furto ou uso indevido desta
+        informação por parte de outros Utilizadores ou por parte de terceiros. É
+        dever do Utilizador fazer o Logout (Desligar) a sua conta para terminar
+        a sua sessão no Plataforma Ortoprotesia e proteger os seus dados.
       </p>
     </div>
     <Footer />
