5f6abe79b6303a25643ddae75be8df85e52d2272
Nelio Bila <neliobilla@gmail.com>
2023-02-05 18:34:44 +0200

    fix: terms and conditions

--- resources/js/pages/legal/TermsConditions.vue
+++ resources/js/pages/legal/TermsConditions.vue
@@ -50,98 +50,104 @@
         mesma é disponibilizada na plataforma imediatamente e sem a intervenção
         de terceiros. Toda a informação disponibilizada relativa ao Profissional
         de Saúde e aos seus serviços é da inteira responsabilidade do mesmo. No
-        entanto, a equipa de gestão do Biscate reserva-se o direito de rever o
-        comentário e omitir termos e expressões ofensivas, sem alterar o seu
-        sentido, onde for possível. Não é permitido a nenhum Utilizador do
-        Biscate utilizar a plataforma e seus serviços e utilidades para abusar,
-        difamar, ameaçar, intimidar e/ou usurpar a identidade de um outro. Em
-        qualquer destes casos, o autor sujeita-se a ter os seus comentários
-        removidos por completo, ou a ser removido e bloqueado do serviço e
-        plataforma, sem aviso prévio do Biscate. A equipa de gestão tomou
-        medidas para garantir a actualidade, disponibilidade e integridade das
-        informações contidas no Biscate "tal como está". A equipa de gestão não
-        oferece qualquer garantia ou assume quaisquer responsabilidades sobre os
-        conteúdos expressos ou implícitos do Biscate. PROPRIEDADE DOS CONTEÚDOS
-        Todos os conteúdos do aplicativo móvel, website e estratégia de
-        comunicação do Biscate (marcas, logótipos, design, texto, grafismo e
-        interface de utilização) estão protegidos por Direitos de Autor e
-        Relacionados, Direitos de Propriedade Industrial e pela Lei da
-        Criminalidade Informática (se já em vigor). Os logótipos e marcas das
-        empresas e entidades que irão publicar Anúncios no Biscate são
+        entanto, a equipa de gestão da Plataforma Ortoprotesia reserva-se o
+        direito de rever o comentário e omitir termos e expressões ofensivas,
+        sem alterar o seu sentido, onde for possível. Não é permitido a nenhum
+        Utilizador da Plataforma Ortoprotesia utilizar a plataforma e seus
+        serviços e utilidades para abusar, difamar, ameaçar, intimidar e/ou
+        usurpar a identidade de um outro. Em qualquer destes casos, o autor
+        sujeita-se a ter os seus comentários removidos por completo, ou a ser
+        removido e bloqueado do serviço e plataforma, sem aviso prévio da
+        Plataforma Ortoprotesia. A equipa de gestão tomou medidas para garantir
+        a actualidade, disponibilidade e integridade das informações contidas no
+        Plataforma Ortoprotesia "tal como está". A equipa de gestão não oferece
+        qualquer garantia ou assume quaisquer responsabilidades sobre os
+        conteúdos expressos ou implícitos da Plataforma Ortoprotesia.
+      </p>
+      <p>
+        <span class="fw-bold">PROPRIEDADE DOS CONTEÚDOS</span>
+        Todos os conteúdos do website e estratégia de comunicação da Plataforma
+        Ortoprotesia (marcas, logótipos, design, texto, grafismo e interface de
+        utilização) estão protegidos por Direitos de Autor e Relacionados,
+        Direitos de Propriedade Industrial e pela Lei da Criminalidade
+        Informática (se já em vigor). Os logótipos e marcas das empresas e
+        entidades que irão publicar Anúncios no Plataforma Ortoprotesia são
         protegidos por Direitos de Autor e outros Direitos das respectivas
         empresas e entidades e serão utilizados com o seu consentimento e para o
-        propósito de se identificarem os autores dos Anúncios. ANÚNCIOS O
-        Biscate vende espaços para a inserção de Anúncios por parte dos
-        Anunciantes. A publicação de Anúncios no Biscate está dependente da
-        revisão, aprovação e pagamento do serviço. A equipa de gestão reserva-se
-        o direito de recusar a prestação deste serviço a quem entender, sem
-        precisar de apresentar justificações para tal. O Anunciante deve, de
-        maneira a receber a aprovação de prestação de serviço, aceitar estes
-        Termos e Condições e seguir os deveres enumerados a seguir: O Anunciante
-        é o único responsável pela autoria intelectual e direitos de uso de
-        todos os materiais publicados no Anúncio, incluindo imagens, marcas,
-        logótipos e textos. O Biscate não se responsabiliza por quaisquer danos
-        causados pela publicação do conteúdo pago para divulgação pelo
-        Anunciante. Em caso de uso de material não autorizado, o contrato de
-        prestação de serviços é revogado e o Biscate não é obrigado a reembolsar
-        o Anunciante; A equipa de gestão não se responsabiliza por falhas de
+        propósito de se identificarem os autores dos Anúncios.
+      </p>
+      <p>
+        <span class="fw-bold">ANÚNCIOS</span>
+        A Plataforma Ortoprotesia vende espaços para a inserção de Anúncios por
+        parte dos Anunciantes. A publicação de Anúncios no Plataforma
+        Ortoprotesia está dependente da revisão, aprovação e pagamento do
+        serviço. A equipa de gestão reserva-se o direito de recusar a prestação
+        deste serviço a quem entender, sem precisar de apresentar justificações
+        para tal. O Anunciante deve, de maneira a receber a aprovação de
+        prestação de serviço, aceitar estes Termos e Condições e seguir os
+        deveres enumerados a seguir: O Anunciante é o único responsável pela
+        autoria intelectual e direitos de uso de todos os materiais publicados
+        no Anúncio, incluindo imagens, marcas, logótipos e textos. A Plataforma
+        Ortoprotesia não se responsabiliza por quaisquer danos causados pela
+        publicação do conteúdo pago para divulgação pelo Anunciante. Em caso de
+        uso de material não autorizado, o contrato de prestação de serviços é
+        revogado e a Plataforma Ortoprotesia não é obrigada a reembolsar o
+        Anunciante; A equipa de gestão não se responsabiliza por falhas de
         funcionamento, danos causados pelo tráfego ou qualquer outra
         inconveniência causada pela ligação gerada pelo Anúncio. A equipa de
         gestão não garante nem pode garantir um número fixo de impressões,
-        publicações, conversões ou cliques em qualquer Anúncio publicado no
-        Biscate. A equipa de gestão reserva-se o direito de remover o Anúncio a
-        qualquer altura, caso o Anunciante infrinja os direitos de autor de
-        terceiros ou os Termos e Condições aqui descritos, sem ser obrigado a
-        reembolsar o Anunciante. Após a aprovação da prestação do serviço, é
-        dever do Anunciante proceder ao pagamento dentro do prazo estipulado
-        pela equipa de gestão do Biscate. Falha no cumprimento do pagamento irá
-        resultar no cancelamento da prestação do serviço. O serviço só será
-        prestado após pagamento e terá a duração estipulada pela equipa de
-        gestão do Biscate e pelo Cliente. Não é permitida a extensão da
-        prestação de serviço para além das datas estipuladas no contrato. O
-        Cliente deverá proceder à requisição de um novo contrato de prestação de
-        serviço. Toda a informação contida no Anúncio é da inteira e única
-        responsabilidade do Anunciante, e o Biscate não se responsabiliza pela
+        publicações, conversões ou cliques em qualquer Anúncio publicado na
+        Plataforma Ortoprotesia. A equipa de gestão reserva-se o direito de
+        remover o Anúncio a qualquer altura, caso o Anunciante infrinja os
+        direitos de autor de terceiros ou os Termos e Condições aqui descritos,
+        sem ser obrigado a reembolsar o Anunciante. Após a aprovação da
+        prestação do serviço, é dever do Anunciante proceder ao pagamento dentro
+        do prazo estipulado pela equipa de gestão da Plataforma Ortoprotesia.
+        Falha no cumprimento do pagamento irá resultar no cancelamento da
+        prestação do serviço. O serviço só será prestado após pagamento e terá a
+        duração estipulada pela equipa de gestão da Plataforma Ortoprotesia e
+        pelo Cliente. Não é permitida a extensão da prestação de serviço para
+        além das datas estipuladas no contrato. O Cliente deverá proceder à
+        requisição de um novo contrato de prestação de serviço. Toda a
+        informação contida no Anúncio é da inteira e única responsabilidade do
+        Anunciante, e a Plataforma Ortoprotesia não se responsabiliza pela
         veracidade da informação, condições de eligibilidade, etc. No entanto, a
         equipa de gestão reserva-se o direito de remover o Anúncio caso alguma
         informação seja falsa, fraudulenta e/ou enganosa. Anúncios divulgados
-        por SMS são serviços activados por opção do Utilizador e estes só serão
-        divulgados pelos contactos que optaram voluntariamente por receber estes
-        Anúncios. Anúncios no website e aplicativo móvel não são opcionais
-        PARTILHA DE DADOS A equipa de gestão do Biscate reserva-se ao direito,
+        por email são serviços activados por opção do Utilizador e estes só
+        serão divulgados pelos contactos que optaram voluntariamente por receber
+        estes Anúncios. Anúncios no website e aplicativo móvel não são opcionais
+      </p>
+
+      <p>
+        <span class="fw-bold">PARTILHA DE DADOS</span>
+        A equipa de gestão da Plataforma Ortoprotesia reserva-se ao direito,
         para efeitos de monitoria e avaliação e melhoria do serviço, de
         partilhar, com entidades designadas e sob um memorando de entendimento,
         dados demográficos, tais como género, idade e localização e dados de
-        utilização, tais como os processos mais utilizados (SMS, chamadas, USSD,
-        e-mail, Facebook, Whatsapp) e frequência de recarregamentos e utilização
-        de dados móveis, dos utilizadores desta plataforma, sem prejuízo na sua
-        utilização e de forma anónima. EXONERAÇÃO DE RESPONSABILIDADE O Biscate
-        serve para a divulgação, selecção, obtenção do contacto do Trabalhador e
-        avaliação dos serviços prestados e publicação de Anúncios relevantes. O
-        Biscate e a sua equipa de gestão não são de forma alguma responsáveis
-        pelo processo de contratação do Trabalhador pelo Cliente, da prestação
-        de serviços do Trabalhador para o Cliente e do pagamento do Cliente ao
-        Trabalhador por esses serviços. A equipa de gestão do Biscate não se
-        responsabiliza em caso de trabalhos de baixa qualidade e/ ou
-        incompletos, furto, burla, ou qualquer outra situação incorrecta ou
-        ilegal, sendo toda a utilização de contactos dentro ou fora da
-        plataforma da inteira responsabilidade dos Utilizadores. Qualquer caso
-        legal que advenha da utilização implícita ou explícita do Biscate deverá
-        ser tratado pelas Autoridades Competentes, sem o envolvimento ou
-        responsabilidade do Biscate ou seus Parceiros. SUSPENSÃO OU INTERRUPÇÃO
-        A equipa do Biscate reserva-se o direito, segundo critérios próprios,
-        de, a qualquer momento, e sem necessidade de aviso prévio, bem como sem
-        qualquer responsabilidade sobre eventuais prejuízos, de interromper,
-        suspender, modificar ou mesmo cessar o serviço prestado pelo Biscate.
-        ALTERAÇÕES NOS TERMOS E CONDIÇÕES A equipa de gestão reserva-se o
-        direito de modificar os Termos e Condições aqui descritos, a qualquer
-        momento e sem aviso prévio. As alterações serão apresentadas nesta
-        página, com indicação da data de revisão e entram imediatamente em
-        vigor. Caso não concorde com os Termos e Condições aqui descritos ou com
-        modificações, deve de imediato cessar a utilização do Biscate. O uso
-        continuado do Biscate agora e após qualquer alteração, expressa a sua
-        aceitação dos Termos e Condições na sua totalidade e de qualquer mudança
-        e estabelece um novo vínculo legal.
+        utilização, tais como os processos mais utilizados ( e-mail, Facebook,
+        Whatsapp) e frequência de utilização de dados móveis, dos utilizadores
+        desta plataforma, sem prejuízo na sua utilização e de forma anónima.
+      </p>
+      <p>
+        <span class="fw-bold">SUSPENSÃO OU INTERRUPÇÃO</span>
+        A equipa da Plataforma Ortoprotesia reserva-se o direito, segundo
+        critérios próprios, de, a qualquer momento, e sem necessidade de aviso
+        prévio, bem como sem qualquer responsabilidade sobre eventuais
+        prejuízos, de interromper, suspender, modificar ou mesmo cessar o
+        serviço prestado pelo Plataforma Ortoprotesia.
+      </p>
+      <p>
+        <span class="fw-bold">ALTERAÇÕES NOS TERMOS E CONDIÇÕES</span>
+        A equipa de gestão reserva-se o direito de modificar os Termos e
+        Condições aqui descritos, a qualquer momento e sem aviso prévio. As
+        alterações serão apresentadas nesta página, com indicação da data de
+        revisão e entram imediatamente em vigor. Caso não concorde com os Termos
+        e Condições aqui descritos ou com modificações, deve de imediato cessar
+        a utilização da Plataforma Ortoprotesia. O uso continuado da Plataforma
+        Ortoprotesia agora e após qualquer alteração, expressa a sua aceitação
+        dos Termos e Condições na sua totalidade e de qualquer mudança e
+        estabelece um novo vínculo legal.
       </p>
     </div>
     <Footer />
